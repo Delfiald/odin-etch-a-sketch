ceade2dc3b3469167d4da367e77c44f15c825fc6
M.Aldi Gunawan <aldigunawan1808@gmail.com>
2024-07-03 17:02:52 +0700

    Adding Draw Logic
    -adding draw logic so can draw by hold and drag
    -adding color pick
    -adding eraser
    
    -need to work for draw logic because now you need to
    move mouse draw, you can't just click

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-console.log('Hello World');
+const container = document.querySelector("#container");
+const wrapper = container.querySelector(".wrapper");
+const erase = document.querySelector(".eraser");
+const colorPicker = document.querySelector("#color-picker");
+
+const div = [];
+
+let divItem = 3500;
+
+for(let i = 0; i < divItem; i++){
+  div[i] = document.createElement("div");
+  wrapper.appendChild(div[i]);
+}
+
+let isDragged = false;
+let isErase = false;
+
+
+div.forEach((item, index) => {
+  item.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    isDragged = true;
+  })
+
+  item.addEventListener('mouseup', (e) => {
+    isDragged = false;
+  })
+
+  item.addEventListener('mouseenter', (e) => {
+    if(isDragged){
+      item.style.background = colorPicker.value;
+    }
+  })
+
+  erase.addEventListener('click', () => {
+    item.style.background = '#fff';
+  })
+})
