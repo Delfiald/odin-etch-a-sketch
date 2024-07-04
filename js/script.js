3b9056fffbb0f5e6946c36f1e57bcd2812433707
M.Aldi Gunawan <aldigunawan1808@gmail.com>
2024-07-04 18:45:40 +0700

    Adding Drawer Options
    adding drawer size options that can be choose by
    clicking button
    
    - 16x16
    - 32x32
    - asking user size
    - free drawing with max size 960px

--- js/script.js
+++ js/script.js
@@ -1,1 +1,51 @@
-console.log('Hello World');
+const drawer = document.querySelector('#drawer');
+const size16 = document.querySelector('#x-16');
+const size32 = document.querySelector('#x-32');
+const sizeX = document.querySelector('#x-size');
+const freeDrawing = document.querySelector('#free-drawing');
+
+const boxes = [];
+
+const setDrawer = (drawerSize) => {
+  for(let x = 0; x < drawerSize; x++){
+    for(let y = 0; y < drawerSize; y++){
+      boxes[y] = document.createElement('div');
+      boxes[y].style.flex = `1 1 ${100/drawerSize}%`;
+      boxes[y].style.width = `${75/drawerSize}vh`;
+      drawer.append(boxes[y]);
+    }
+  }
+}
+
+setDrawer(16);
+
+size16.addEventListener('click', (e) => {
+  drawer.innerHTML = '';
+  setDrawer(16);
+})
+
+size32.addEventListener('click', (e) => {
+  drawer.innerHTML = '';
+  setDrawer(32);
+})
+
+sizeX.addEventListener('click', (e) => {
+  drawer.innerHTML = '';
+  setDrawer(prompt('Set Size (Max 100)'));
+})
+
+freeDrawing.addEventListener('click', (e) => {
+  drawer.innerHTML = '';
+  const containerHeight = drawer.clientHeight;
+  const boxSize = 5;
+
+  const numberOfColumns = Math.floor(1100 / boxSize);
+  const numberOfRows = Math.floor(containerHeight / boxSize);
+  const totalBoxes = numberOfColumns * numberOfRows;
+  console.log(totalBoxes);
+
+  for (let i = 0; i < totalBoxes; i++) {
+    const box = document.createElement('div');
+    drawer.appendChild(box);
+  }
+})
